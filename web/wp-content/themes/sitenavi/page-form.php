ce73062c44f2a66f5767934e2453f0efe1b9ebc0
sunriseezawa <ezawa@sunrisedigital.jp>
2014-02-05 16:05:16 +0900

    web広告サイトコーディング

---
+++ web/wp-content/themes/sitenavi/page-form.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: page-form
+*/
+?>
+		<?php get_header(); ?>
+
+<iframe src="https://docs.google.com/forms/d/16aaO3L3nSSUFjYFQjasWM9OfJ-mHrDGfZJv6fwouVwc/viewform?embedded=true#start=embed" width="760" height="405" frameborder="0" marginheight="0" marginwidth="0">読み込み中...</iframe>
+
+		<?php get_footer(); ?>
