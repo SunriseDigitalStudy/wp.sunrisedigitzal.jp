a693d97ba52b9514d06c6dd012298ef6f2ad7d8f
sunriseezawa <ezawa@sunrisedigital.jp>
2014-03-28 19:14:09 +0900

    ヘッダー周りコーディング

--- web/wp-content/themes/sitenavi/header.php
+++ web/wp-content/themes/sitenavi/header.php
@@ -18,18 +18,17 @@
 <body <?php body_class(); ?>>
 	<div id="header">
 		<div class="container">
-			<h1><a href="http://wp.sunrisedigital.jp/"><img src="<?php bloginfo('template_directory');?>/images/logo.jpg" alt="" /></a></h1>
-			<ul id="header-toplist">
-				<li><a href="/new/">新着情報</a></li>
-				<li><a href="/form/">問い合わせフォーム</a></li>
-			</ul>
+			<h1><a href="http://wp.sunrisedigital.jp/"><img src="<?php bloginfo('template_directory');?>/images/logo.gif" alt="SiteNavi" /></a></h1>
+			<p id="form">
+				<a href="/form/">お問い合わせ</a>
+			</p>
 			<ul id="header-navi">
-				<li class="baitai"><a href="" data-btn="trigger" data-sync="trigger-action">媒体解説</a></li>
-				<li><a href="/downloads/">資料ダウンロード</a></li>
-				<li><a href="/price/">料金</a></li>
-				<li><a href="/blog/">ブログ</a></li>
+				<li class="baitai"><a href="" data-btn="trigger" data-sync="trigger-action">媒体解説</a></li><!--
+				--><li><a href="/new/">新着情報</a></li><!--
+				--><li><a href="/downloads/">資料ダウンロード</a></li><!--
+				--><li><a href="/price/">料金一覧</a></li>
 			</ul>
-			<div id="navi-site" class="arrow-box" data-sync-receive="trigger-action">
+			<div id="navi-site" data-sync-receive="trigger-action">
 				<ul>
 					<li><a href="/nukinavi/">ヌキなび</a></li>
 					<li><a href="/furonavi/">フロなび</a></li>
@@ -41,8 +40,8 @@
 			</div>
 		</div>
 	</div>
-	<div id="title-box">
 		<?php if ( !is_home() && !is_front_page() ) : ?>
+		<div id="title-box">
 			<div>
 				<h2><?php the_title(); ?></h2>
 			</div>
@@ -53,8 +52,8 @@
 					<?php include( TEMPLATEPATH . '/menu-site.php' ); ?>				
 				<?php endif; ?>		
 			<?php endif; ?>	
+		</div>
 		<?php endif; ?>
-	</div>
 	<div class="backtotop" align="center">
 		<a href="#header">トップに<br>戻る</a>
 	</div>
